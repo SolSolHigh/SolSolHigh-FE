8d5e40b5873c86c9418f905b647320addcc4d454
bluewhaleyh <bluewhaleyh@gmail.com>
2024-08-18 03:23:12 +0900

    chore/#8: eslint, prettier 적용

--- ssh-web/src/App.tsx
+++ ssh-web/src/App.tsx
@@ -1,20 +1,20 @@
-import "./App.css";
-import axios from "axios";
-import { useEffect } from "react";
+import './App.css';
+import axios from 'axios';
+import { useEffect } from 'react';
 
 function App() {
-    useEffect(() => {
-        axios.get("/users").then((res) => {
-            console.log(res.data);
-        });
-    }, []);
+  useEffect(() => {
+    axios.get('/users').then((res) => {
+      console.log(res.data);
+    });
+  }, []);
 
-    return (
-        <div className="flex font-bold">
-            <div>안뇽</div>&nbsp;
-            <div className="font-thin">tailwind</div>
-        </div>
-    );
+  return (
+    <div className="flex font-bold">
+      <div>안뇽</div>&nbsp;
+      <div className="font-thin">tailwind</div>
+    </div>
+  );
 }
 
 export default App;
