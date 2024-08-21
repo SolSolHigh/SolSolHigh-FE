2a5ce424c1738dffebc0def39875f27bf35c3981
bluewhaleyh <bluewhaleyh@gmail.com>
2024-08-22 01:23:46 +0900

    fix/#26: platform 지정 방식 수정
    recoil 을 이용한 전역 관리

--- ssh-web/src/App.tsx
+++ ssh-web/src/App.tsx
@@ -1,11 +1,14 @@
 import './App.css';
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { useSuspenseQuery } from '@tanstack/react-query';
 import { getExample2 } from './apis/exampleApi';
 import { Route, Routes } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { Login } from './pages/Login';
 import { Signup } from './pages/Signup';
+import { useRecoilState } from 'recoil';
+import { platformState } from './atoms/platformAtom';
+import { TPlatform } from './themes/themeBase';
 
 function App() {
   /*const {
@@ -33,15 +36,28 @@ function App() {
   }
   // ============= Example Test ======================
 
-  return (
-    <Routes>
-      {/* 홈화면 */}
-      <Route path="/" element={<Home />} />
+  const appRef = useRef<HTMLDivElement>(null);
+  const [platform, setPlatform] = useRecoilState<TPlatform>(platformState);
 
-      {/* 인증/인가 */}
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<Signup />} />
-    </Routes>
+  useEffect(() => {
+    if (appRef.current) {
+      if (appRef.current.offsetWidth >= 1280) setPlatform('W');
+      else if (appRef.current.offsetWidth >= 768) setPlatform('T');
+      else setPlatform('M');
+    }
+  }, []);
+
+  return (
+    <div ref={appRef} className="w-full h-full">
+      <Routes>
+        {/* 홈화면 */}
+        <Route path="/" element={<Home />} />
+
+        {/* 인증/인가 */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </div>
   );
 }
 
--- ssh-web/src/pages/Login/index.tsx
+++ ssh-web/src/pages/Login/index.tsx
@@ -1,31 +1,31 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React from 'react';
 import { Typography } from '../../components/atoms/Typography';
 import { LoginNav } from '../../components/molecules/LoginNav';
+import { useRecoilValue } from 'recoil';
+import { platformState } from '../../atoms/platformAtom';
 
 export const Login = () => {
-  const pageRef = useRef<HTMLDivElement>(null);
-  const [isDesktop, setIsDesktop] = useState<boolean>(true);
-
-  useEffect(() => {
-    if (pageRef.current) {
-      setIsDesktop(pageRef.current.offsetWidth >= 1280);
-    }
-  }, []);
+  const platform = useRecoilValue(platformState);
 
   return (
-    <div
-      ref={pageRef}
-      className="flex flex-col items-center w-full h-full desktop:justify-center tablet:mt-40"
-    >
-      <Typography color="dark" weight="bold" size={isDesktop ? '7xl' : 'xl'}>
+    <div className="flex flex-col items-center w-full h-full desktop:justify-center tablet:mt-40">
+      <Typography
+        color="dark"
+        weight="bold"
+        size={platform === 'W' ? '7xl' : 'xl'}
+      >
         쏠쏠-하이
       </Typography>
-      <Typography color="dark" weight="medium" size={isDesktop ? '2xl' : 'md'}>
+      <Typography
+        color="dark"
+        weight="medium"
+        size={platform === 'W' ? '2xl' : 'md'}
+      >
         반가워요! 찾아와줘서 고마워요
       </Typography>
       <img
         src="/assets/mascot_top.png"
-        className={`mt-4 ${isDesktop ? '' : 'w-80'}`}
+        className={`mt-4 ${platform === 'W' ? '' : 'w-80'}`}
       />
       <LoginNav />
     </div>
