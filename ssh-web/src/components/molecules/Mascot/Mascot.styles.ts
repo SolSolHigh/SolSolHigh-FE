665105b57c257b6b9090a436c7a913dd48221838
bluewhaleyh <bluewhaleyh@gmail.com>
2024-08-22 02:31:51 +0900

    feat/#26: 회원가입 페이지 프레임 디자인
    마스코트 & 컨텐츠 컨테이너 디자인

--- ssh-web/src/components/molecules/Mascot/Mascot.styles.ts
+++ ssh-web/src/components/molecules/Mascot/Mascot.styles.ts
@@ -1,7 +1,7 @@
 import { tv } from 'tailwind-variants';
 
 export const mascotStyles = tv({
-  base: 'flex justify-center desktop:flex-col desktop:items-center desktop:gap-y-12 tablet:items-end tablet:gap-x-2',
+  base: 'flex justify-center desktop:flex-col desktop:items-center desktop:gap-y-12 tablet:items-end tablet:gap-x-2 tablet:mt-8 tablet:mb-4',
 });
 
 export const mascotImgStyles = tv({
---
+++ ssh-web/src/pages/Signup/style.ts
@@ -0,0 +1,5 @@
+import { tv } from 'tailwind-variants';
+
+export const contentStyles = tv({
+  base: 'desktop:w-[48rem] desktop:h-[48rem] desktop:rounded-lg tablet:w-full tablet:h-full tablet:rounded-t-[2rem] bg-white',
+});
