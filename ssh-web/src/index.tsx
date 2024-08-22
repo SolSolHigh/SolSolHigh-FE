57f1f9081ad63ddfb88cba471bf43690e2815e7b
horiz.d <65459616+ABizCho@users.noreply.github.com>
2024-08-23 00:23:28 +0900

    feat#23-modal: CreateBrowserRouter 기반 라우터 세팅
    1. router.tsx에 라우터 설정
    2. PathName 타입 설정

--- ssh-web/src/index.tsx
+++ ssh-web/src/index.tsx
@@ -1,60 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { RecoilRoot } from 'recoil';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './utils/router';
 
 if (process.env.NODE_ENV === 'development') {
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
   require('./mocks');
 }
 
-const PathNames = {
-  HOME: '/home',
-  MISSION: '/mission',
-  QUIZ: '/quiz',
-  EGG: '/egg',
-  MYPAGE: '/mypage',
-};
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    errorElement: <></>,
-    children: [
-      {
-        index: true,
-        path: '/',
-        element: <></>,
-      },
-      {
-        path: PathNames.HOME,
-        element: <></>,
-      },
-      {
-        path: PathNames.MISSION,
-        element: <></>,
-      },
-      {
-        path: PathNames.QUIZ,
-        element: <></>,
-      },
-      {
-        path: PathNames.EGG,
-        element: <></>,
-      },
-      {
-        path: PathNames.MYPAGE,
-        element: <></>,
-      },
-    ],
-  },
-]);
 const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(
---
+++ ssh-web/src/utils/router.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { createBrowserRouter } from 'react-router-dom';
+import App from '../App';
+import Home from '../pages/Home';
+import { IPathNames } from '../interfaces/routerInterface';
+
+export const PathNames: IPathNames = {
+  HOME: {
+    path: '/home',
+    name: '홈',
+  },
+  MISSION: {
+    path: '/mission',
+    name: '미션',
+  },
+  QUIZ: { path: '/quiz', name: '퀴즈' },
+  EGG: { path: '/egg', name: '계란' },
+  MYPAGE: {
+    path: '/mypage',
+    name: '전체',
+  },
+};
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <></>,
+    children: [
+      {
+        index: true,
+        path: '/',
+        element: <></>,
+      },
+      {
+        path: PathNames.HOME.path,
+        element: <Home />,
+      },
+      {
+        path: PathNames.MISSION.path,
+        element: <></>,
+      },
+      {
+        path: PathNames.QUIZ.path,
+        element: <></>,
+      },
+      {
+        path: PathNames.EGG.path,
+        element: <></>,
+      },
+      {
+        path: PathNames.MYPAGE.path,
+        element: <></>,
+      },
+    ],
+  },
+]);
