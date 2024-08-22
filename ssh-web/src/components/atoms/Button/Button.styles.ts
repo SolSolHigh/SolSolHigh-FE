81047d0e9c6cc3a1f30991910a8d457ba42d0cdc
horiz.d <65459616+ABizCho@users.noreply.github.com>
2024-08-23 03:34:44 +0900

    feat/#30-button-extend: 색상 조합 함수 제거, stories 문서 추가 1. tailwind의 JIT 모드 평가방식으로 동적 tailwind 클래스네임 조합 함수 사용 불가로 제거, 복구 2. stoires 문서 추가 ( color, size, outlined, rounded, fullWidth )

--- ssh-web/src/components/atoms/Button/Button.styles.ts
+++ ssh-web/src/components/atoms/Button/Button.styles.ts
@@ -1,15 +1,38 @@
 import { tv } from 'tailwind-variants';
 
-const createButtonColorVariants = (color: string) => ({
-  normal: `bg-${color}-500 hover:bg-${color}-400 active:bg-${color}-700 text-white`,
-  outlined: `border-2 border-${color}-500 hover:border-${color}-600 active:border-${color}-700 text-${color}-500 hover:text-${color}-700 active:text-${color}-900 hover:bg-${color}-50 active:bg-${color}-100`,
-});
-
 const variants = {
-  primary: createButtonColorVariants('primary'),
-  secondary: createButtonColorVariants('secondary'),
-  danger: createButtonColorVariants('danger'),
-  warning: createButtonColorVariants('warning'),
+  blue: {
+    normal: 'bg-blue-500 hover:bg-blue-400 active:bg-blue-700 text-white',
+    outlined:
+      'border-2 border-blue-500 hover:border-blue-600 active:border-blue-700 text-blue-500 hover:text-blue-700 active:text-blue-900 hover:bg-blue-50 active:bg-blue-100',
+  },
+  gray: {
+    normal: 'bg-gray-500 hover:bg-gray-400 active:bg-gray-600 text-white',
+    outlined:
+      'border-2 border-gray-500 hover:border-gray-600 active:border-gray-700 text-gray-500 hover:text-gray-700 active:text-gray-900 hover:bg-gray-50 active:bg-gray-100',
+  },
+  primary: {
+    normal:
+      'bg-primary-500 hover:bg-primary-400 active:bg-primary-700 text-white',
+    outlined:
+      'border-2 border-primary-500 hover:border-primary-600 active:border-primary-700 text-primary-500 hover:text-primary-700 active:text-primary-900 hover:bg-primary-50 active:bg-primary-100',
+  },
+  secondary: {
+    normal:
+      'bg-secondary-500 hover:bg-secondary-400 active:bg-secondary-700 text-white',
+    outlined:
+      'border-2 border-secondary-500 hover:border-secondary-600 active:border-secondary-700 text-secondary-500 hover:text-secondary-700 active:text-secondary-900 hover:bg-secondary-50 active:bg-secondary-100',
+  },
+  danger: {
+    normal: 'bg-red-500 hover:bg-red-400 active:bg-red-700 text-white',
+    outlined:
+      'border-2 border-red-500 hover:border-red-600 active:border-red-700 text-red-500 hover:text-red-700 active:text-red-900 hover:bg-red-50 active:bg-red-100',
+  },
+  warning: {
+    normal: 'bg-yellow-500 hover:bg-yellow-400 active:bg-yellow-700 text-white',
+    outlined:
+      'border-2 border-yellow-500 hover:border-yellow-600 active:border-yellow-700 text-yellow-500 hover:text-yellow-700 active:text-yellow-900 hover:bg-yellow-50 active:bg-yellow-100',
+  },
   dark: {
     normal: 'bg-black hover:bg-gray-800 active:bg-gray-900 text-white',
     outlined:
@@ -24,7 +47,7 @@ const variants = {
 };
 
 export const buttonStyles = tv({
-  base: 'w-max flex items-center justify-center rounded-md focus:outline-none transition-all duration-500 shadow-sm hover:shadow-md active:shadow-sm',
+  base: 'w-max flex items-center justify-center rounded-md focus:outline-none transition duration-500 shadow-sm hover:shadow-md active:shadow-sm',
   variants: {
     bg: {
       primary: variants.primary.normal,
@@ -62,8 +85,8 @@ export const buttonStyles = tv({
       disabled: variants.disabled,
     },
     size: {
-      sm: 'px-4 py-2 text-sm max-h-10',
-      md: 'px-5 py-2 max-h-12',
+      sm: 'px-3 py-2 text-sm max-h-10',
+      md: 'px-4 py-2 max-h-12',
       lg: 'px-6 py-3 max-h-14 text-lg',
     },
     rounded: {
