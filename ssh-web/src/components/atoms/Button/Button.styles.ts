d27be8c6b381d557e5cd589d78756b0c78d7d555
horiz.d <65459616+ABizCho@users.noreply.github.com>
2024-08-23 02:20:32 +0900

    feat/#30-button-extend: 버튼 Atom 컴포넌트 확장 및 리팩토링 >> >> 1. 정의된 Color Palette에 맞게 colorVariants 확장 >> 2. 버튼 disabled 색상 처리 >> 3. 버튼 전용 colorVariants 자동 생성하는 유틸함수 개발 >>

--- ssh-web/src/components/atoms/Button/Button.styles.ts
+++ ssh-web/src/components/atoms/Button/Button.styles.ts
@@ -1,61 +1,83 @@
-import { tv } from "tailwind-variants";
+import { tv } from 'tailwind-variants';
 
-// primary, secondary 등 palette 정의되면 스타일 수정해야함
+const createButtonColorVariants = (color: string) => ({
+  normal: `bg-${color}-500 hover:bg-${color}-400 active:bg-${color}-700 text-white`,
+  outlined: `border-2 border-${color}-500 hover:border-${color}-600 active:border-${color}-700 text-${color}-500 hover:text-${color}-700 active:text-${color}-900 hover:bg-${color}-50 active:bg-${color}-100`,
+});
+
 const variants = {
-  blue: {
-    normal: "bg-blue-500 hover:bg-blue-400 active:bg-blue-700 text-white",
+  primary: createButtonColorVariants('primary'),
+  secondary: createButtonColorVariants('secondary'),
+  danger: createButtonColorVariants('danger'),
+  warning: createButtonColorVariants('warning'),
+  dark: {
+    normal: 'bg-black hover:bg-gray-800 active:bg-gray-900 text-white',
     outlined:
-      "border-2 border-blue-500 hover:border-blue-600 active:border-blue-700 text-blue-500 hover:text-blue-700 active:text-blue-900 hover:bg-blue-50 active:bg-blue-100",
+      'border-2 border-black hover:border-gray-800 active:border-gray-900 text-black hover:text-gray-700 active:text-gray-900 hover:bg-gray-100 active:bg-gray-200',
   },
-
-  gray: {
-    normal: "bg-gray-500 hover:bg-gray-400 active:bg-gray-600 text-white",
+  light: {
+    normal: 'bg-white hover:bg-gray-100 active:bg-gray-200 text-black',
     outlined:
-      "border-2 border-gray-500 hover:border-gray-600 active:border-gray-700 text-gray-500 hover:text-gray-700 active:text-gray-900 hover:bg-gray-50 active:bg-gray-100",
+      'border-2 border-white hover:border-gray-100 active:border-gray-200 text-white hover:text-gray-500 active:text-gray-700 hover:bg-gray-50 active:bg-gray-100',
   },
-  none: {
-    normal: "bg-transparent",
-    outlined: "border-2 border-transparent hover:border-gray-200 active:border-gray-400",
-  },
+  disabled: 'bg-secondary-300 text-white cursor-not-allowed',
 };
 
 export const buttonStyles = tv({
-  base: "w-max flex items-center justify-center rounded-md focus:outline-none transition duration-500 shadow-sm hover:shadow-md active:shadow-sm",
+  base: 'w-max flex items-center justify-center rounded-md focus:outline-none transition-all duration-500 shadow-sm hover:shadow-md active:shadow-sm',
   variants: {
     bg: {
-      blue: variants.blue.normal,
-      gray: variants.gray.normal,
-      none: variants.none.normal,
+      primary: variants.primary.normal,
+      secondary: variants.secondary.normal,
+      danger: variants.danger.normal,
+      warning: variants.warning.normal,
+      dark: variants.dark.normal,
+      light: variants.light.normal,
+      none: 'bg-transparent',
+      disabled: variants.disabled,
     },
     borderColor: {
-      blue: variants.blue.outlined,
-      gray: variants.gray.outlined,
-      none: variants.none.outlined,
+      primary: variants.primary.outlined,
+      secondary: variants.secondary.outlined,
+      danger: variants.danger.outlined,
+      warning: variants.warning.outlined,
+      dark: variants.dark.outlined,
+      light: variants.light.outlined,
+      none: 'border-2 border-transparent hover:border-gray-200 active:border-gray-400',
     },
     text: {
-      blue: variants.blue.normal,
-      gray: variants.gray.normal,
-      outlined_blue: variants.blue.outlined,
-      outlined_gray: variants.gray.outlined,
-      none: "",
+      primary: variants.primary.normal,
+      secondary: variants.secondary.normal,
+      danger: variants.danger.normal,
+      warning: variants.warning.normal,
+      dark: variants.dark.normal,
+      light: variants.light.normal,
+      outlined_primary: variants.primary.outlined,
+      outlined_secondary: variants.secondary.outlined,
+      outlined_danger: variants.danger.outlined,
+      outlined_warning: variants.warning.outlined,
+      outlined_dark: variants.dark.outlined,
+      outlined_light: variants.light.outlined,
+      none: '',
+      disabled: variants.disabled,
     },
     size: {
-      sm: "px-3 py-2 text-sm max-h-10",
-      md: "px-4 py-2 max-h-12",
-      lg: "px-6 py-3 max-h-14 text-lg",
+      sm: 'px-4 py-2 text-sm max-h-10',
+      md: 'px-5 py-2 max-h-12',
+      lg: 'px-6 py-3 max-h-14 text-lg',
     },
     rounded: {
-      true: "rounded-full",
-      false: "rounded-md",
+      true: 'rounded-full',
+      false: 'rounded-md',
     },
     fullWidth: {
-      true: "w-full",
-      false: "w-max",
+      true: 'w-full',
+      false: 'w-max',
     },
   },
   defaultVariants: {
-    size: "md",
-    bg: "blue",
+    size: 'md',
+    bg: 'primary',
     rounded: false,
     fullWidth: false,
   },
