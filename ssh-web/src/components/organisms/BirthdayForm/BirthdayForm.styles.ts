f02f87043cab1504bc1284a7c7ec092486391c75
bluewhaleyh <bluewhaleyh@gmail.com>
2024-08-23 21:51:26 +0900

    feat/#26: BirthdayForm organisms component 구현
    디자인만 우선 구현

---
+++ ssh-web/src/components/organisms/BirthdayForm/BirthdayForm.styles.ts
@@ -0,0 +1,5 @@
+import { tv } from 'tailwind-variants';
+
+export const containerStyles = tv({
+  base: 'h-full flex flex-col justfiy-between items-center',
+});
---
+++ ssh-web/src/components/organisms/BirthdayForm/index.stories.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import type { Meta, StoryObj } from '@storybook/react';
+import { BirthdayForm } from '.';
+
+const meta = {
+  title: 'UI/Organisms/BirthdayForm',
+  component: BirthdayForm,
+  parameters: {
+    layout: 'fullscreen',
+  },
+  decorators: [
+    (Story) => (
+      <div style={{ padding: '1rem' }}>
+        <Story />
+      </div>
+    ),
+  ],
+  argTypes: {
+    children: {
+      description: '',
+    },
+    classNameStyles: {
+      description: '부가적인 스타일',
+    },
+  },
+} satisfies Meta<typeof BirthdayForm>;
+
+export default meta;
+
+type Story = StoryObj<typeof BirthdayForm>;
+
+export const Default: Story = {
+  args: {},
+  render: () => {
+    return <BirthdayForm />;
+  },
+};
