2456e01235d2ab5c9d61c549f5010e6a8cf7a286
bluewhaleyh <bluewhaleyh@gmail.com>
2024-08-24 23:12:06 +0900

    feat/#26: Signup 페이지 구현
    api 연결 제외 구현 완료

--- ssh-web/src/pages/Signup/index.tsx
+++ ssh-web/src/pages/Signup/index.tsx
@@ -1,18 +1,82 @@
-import React from 'react';
+import React, { ReactNode, useCallback, useState } from 'react';
 import { Mascot } from '../../components/molecules/Mascot';
 import { contentStyles } from './style';
 import { UserInfoForm } from '../../components/organisms/UserInfoForm';
+import {
+  IContent,
+  IContentHandler,
+  ISignupRequest,
+} from '../../interfaces/userInterface';
+import dayjs from 'dayjs';
 
-export const Signup = () => {
+export const Signup = React.memo(() => {
+  const [contents, setContents] = useState<IContent[]>([
+    {
+      key: 'nickname',
+      keyword: '닉네임',
+      ment: '이 뭐에요?',
+      contentType: 'textfield',
+      value: '',
+    },
+    {
+      key: 'birthday',
+      keyword: '생일',
+      ment: '은 언제에요?',
+      contentType: 'textfield',
+      value: dayjs(new Date()).format('YYYY년 MM월 DD일'),
+    },
+    {
+      key: 'gender',
+      keyword: '성별',
+      ment: '은 뭐에요?',
+      contentType: 'buttons',
+      value: '남',
+      valueList: ['남', '여'],
+    },
+    {
+      key: 'type',
+      keyword: '역할',
+      ment: '이 뭐에요?',
+      contentType: 'buttons',
+      value: '부모',
+      valueList: ['부모', '자녀'],
+    },
+  ]);
+  const onContentHandler = useCallback(
+    (idx: number, value: ReactNode) => {
+      setContents((prev) => {
+        const newContents = [...prev];
+        newContents[idx].value = value;
+        return newContents;
+      });
+    },
+    [contents],
+  );
+  const [handler, setHandler] = useState<IContentHandler>({
+    label: '회원가입',
+    handler: () => {
+      const info: ISignupRequest = {
+        nickname: contents[0].value as string,
+        birthday: contents[1].value as string,
+        gender: contents[2].value === '남' ? 'MALE' : 'FEMALE',
+        type: contents[3].value === '부모' ? 'parent' : 'child',
+      };
+      console.log(info);
+    },
+  });
+
   return (
-    <div className="flex items-center justify-center h-full tablet:flex-col">
+    <div className="flex items-center justify-center w-full h-full tablet:flex-col">
       <Mascot nickname="닉네임" ment="반가워요! 몇 가지만 더 물어볼게요" />
       <div className={contentStyles()}>
-        <UserInfoForm />
-        <button className="px-8 py-4 text-white rounded-md bg-primary-500">
-          버튼
-        </button>
+        <UserInfoForm
+          contents={contents}
+          onContentHandler={onContentHandler}
+          handler={handler}
+        />
       </div>
     </div>
   );
-};
+});
+
+Signup.displayName = 'Signup';
