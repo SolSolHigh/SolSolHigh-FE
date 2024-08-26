9c08b45d8b70f7195a1aee79fcc4f6e1ed14a81b
bluewhaleyh <bluewhaleyh@gmail.com>
2024-08-27 00:44:02 +0900

    feat/#50: 네이버 로그인 테스트용 배포

--- ssh-web/src/components/molecules/LoginNav/index.tsx
+++ ssh-web/src/components/molecules/LoginNav/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { LoginNavProps } from './LoginNav.types';
 import { loginNavStyles } from './LoginNav.styles';
-import { login } from '../../../apis/userApi';
 import axios from 'axios';
 
 export const LoginNav = ({ children, classNameStyles }: LoginNavProps) => {
@@ -21,7 +20,18 @@ export const LoginNav = ({ children, classNameStyles }: LoginNavProps) => {
             .catch((err) => console.log(err));
         }}
       />
-      <img src="/assets/kakao_login.png" className="w-full max-w-[22.5rem]" />
+      <img
+        src="/assets/kakao_login.png"
+        className="w-full max-w-[22.5rem]"
+        onClick={async () => {
+          await axios
+            .get(
+              'https://www.solsol-high.kro.kr/api/oauth2/authorization/naver',
+            )
+            .then((res) => console.log(res))
+            .catch((err) => console.log(err));
+        }}
+      />
       {children}
     </div>
   );
