5e6cbea2efad93239dfb1417136598a6fdfd5487
horiz.d <65459616+ABizCho@users.noreply.github.com>
2024-08-27 09:49:52 +0900

    fix/#46: router confilct 수정

--- ssh-web/src/pages/Mission/index.tsx
+++ ssh-web/src/pages/Mission/index.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import { MissionFetch } from './child';
 
 // 리팩토링 필요: 요하 라우터, 서스펜스쿼리 처리에 따라 맞추기
-const Mission = () => {
+export const Mission = () => {
   return <MissionFetch />;
 };
-
-export default Mission;
